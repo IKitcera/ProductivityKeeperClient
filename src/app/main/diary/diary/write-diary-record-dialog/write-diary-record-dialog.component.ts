35ac3977e97bfc8e268bcda6b20fd9f6ec1083bf
Ivanna Kitsera <ivanna.kitsera@intellectsoft.net>
2026-02-08 03:52:41 +0200

    save

--- src/app/main/diary/diary/write-diary-record-dialog/write-diary-record-dialog.component.ts
+++ src/app/main/diary/diary/write-diary-record-dialog/write-diary-record-dialog.component.ts
@@ -6,19 +6,21 @@ import {FormsModule} from "@angular/forms";
 import {QuillEditorComponent} from "ngx-quill";
 
 @Component({
-    selector: 'app-write-diary-record-dialog',
-    imports: [
-        MatFormField,
-        MatLabel,
-        FormsModule,
-        MatInput,
-        QuillEditorComponent
-    ],
-    templateUrl: './write-diary-record-dialog.component.html',
-    styleUrl: './write-diary-record-dialog.component.css'
+  selector: 'app-write-diary-record-dialog',
+  imports: [
+    MatFormField,
+    MatLabel,
+    FormsModule,
+    MatInput,
+    QuillEditorComponent
+  ],
+  templateUrl: './write-diary-record-dialog.component.html',
+  styleUrl: './write-diary-record-dialog.component.css',
+  standalone: true
 })
 export class WriteDiaryRecordDialogComponent {
   previewMode = false;
+
   constructor(
     public dialogRef: MatDialogRef<WriteDiaryRecordDialogComponent>,
     @Inject(MAT_DIALOG_DATA) public data: DiaryRecord) {
